b658bd5c5b59bc609902739d45096cab0cb8b790
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2017-01-07 21:36:05 -0500

    Initial commit

---
+++ forms/Form.swift
@@ -0,0 +1,105 @@
+//
+//  Form.swift
+//  forms
+//
+//  Created by Ricky Joudrey on 1/7/17.
+//  Copyright © 2017 com. All rights reserved.
+//
+
+import UIKit
+
+class Form: NSObject {
+    var sections = [FormSection]()
+}
+
+extension UITableView {
+    func configure(with form: Form) {
+        for section in form.sections {
+            for field in section.fields {
+                field._configurator.registerCell(in: self)
+            }
+        }
+        delegate = form
+        dataSource = form
+        reloadData()
+    }
+}
+
+extension Form: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].fields.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let field = sections[indexPath.section].fields[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: field._configurator.cellType.ky_reuseIdentifier)!
+        field._configurator.configureCell(cell)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+}
+
+extension Form: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let field = sections[indexPath.section].fields[indexPath.row]
+        field.didSelect()
+    }
+}
+
+class FormSection {
+    var fields = [Field]()
+    var title = ""
+}
+
+class Field {
+    fileprivate let _configurator: AnyCellConfigurator
+    
+    // The fact that type-erasure is needed for CellConfigurator is an implementation detail
+    init<C: CellConfigurator>(configurator: C) {
+        self._configurator = AnyCellConfigurator(base: configurator)
+    }
+    
+    func didSelect() {
+        
+    }
+}
+
+extension UITableViewCell {
+    static var ky_reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+protocol CellConfigurator {
+    associatedtype Cell: UITableViewCell
+    func configureCell(_ cell: Cell)
+}
+
+// Erases the type of the cell
+class AnyCellConfigurator: CellConfigurator {
+    let _configure: (UITableViewCell) -> ()
+    let cellType: UITableViewCell.Type
+    
+    init<B: CellConfigurator>(base: B) {
+        cellType = B.Cell.self
+        _configure = { (cell: UITableViewCell) in
+            let typedCell = cell as! B.Cell
+            base.configureCell(typedCell)
+        }
+    }
+    
+    func configureCell(_ cell: UITableViewCell) {
+        _configure(cell)
+    }
+    
+    func registerCell(in tableView: UITableView) {
+        tableView.register(cellType, forCellReuseIdentifier: cellType.ky_reuseIdentifier)
+    }
+}
--- forms/ViewController.swift
+++ forms/ViewController.swift
@@ -8,18 +8,119 @@
 
 import UIKit
 
+class MyCell: UITableViewCell {
+    
+}
+
+class MyOtherCell: UITableViewCell {
+    
+}
+
+// The simplest field. Represents a cell with static data
+class MyField: Field {
+    init() {
+        super.init(configurator: Configurator())
+    }
+    
+    class Configurator: CellConfigurator {
+        func configureCell(_ cell: MyCell) {
+            cell.textLabel?.text = "hello"
+        }
+    }
+}
+
+// The simplest field. Represents a cell with a dataSource and delegate
+class MyOtherField: Field {
+    struct DataSource {
+        let title: String
+        let timesPressed: Int
+    }
+    
+    weak var delegate: MyOtherFieldDelegate?
+    
+    // this field exists so that we can pull data out of the dataSource from the our delegate implementation
+    let configurator: Configurator
+    
+    init(dataSource: DataSource) {
+        configurator = Configurator(dataSource: dataSource)
+        super.init(configurator: configurator)
+    }
+    
+    override func didSelect() {
+        delegate?.didSelectMyOtherField(self)
+    }
+    
+    class Configurator: CellConfigurator {
+        let dataSource: DataSource
+        
+        init(dataSource: DataSource) {
+            self.dataSource = dataSource
+        }
+        
+        func configureCell(_ cell: MyOtherCell) {
+            cell.textLabel?.text = "\(dataSource.title) - \(dataSource.timesPressed)"
+        }
+    }
+}
+
+protocol MyOtherFieldDelegate: class {
+    func didSelectMyOtherField(_ field: MyOtherField)
+}
+
 class ViewController: UIViewController {
+    var tableView: UITableView!
+    
+    var form: Form! {
+        didSet {
+            tableView.configure(with: form)
+        }
+    }
+    
+    func makeForm(timesPressed: Int = 0) -> Form {
+        let form = Form()
+        
+        let section1 = FormSection()
+        section1.title = "Section one"
+        section1.fields = [
+            MyField(),
+            MyField()
+        ]
+        
+        let section2 = FormSection()
+        section2.title = "Section two"
+        
+        let dataSource = MyOtherField.DataSource(title: "Click me!", timesPressed: timesPressed)
+        let selectableField = MyOtherField(dataSource: dataSource)
+        selectableField.delegate = self
+        
+        section2.fields = [
+            MyField(),
+            selectableField
+        ]
+        
+        form.sections = [section1, section2]
+        
+        return form
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        // the form itself is the data source and the delegate
+        form = makeForm()
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 32),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: MyOtherFieldDelegate {
+    func didSelectMyOtherField(_ field: MyOtherField) {
+        form = makeForm(timesPressed: field.configurator.dataSource.timesPressed + 1)
+    }
+}
